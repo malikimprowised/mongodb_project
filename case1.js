931670e3b9a93cff156eb935ead67f0a1c1f73dc
Malik <malik@improwised.com>
2016-02-09 11:29:23 +0530

    commit

--- case1.js
+++ case1.js
@@ -2,97 +2,22 @@ var mongo = require('mongodb').MongoClient;
 var table = require('./display.js');
 var caseTask = process.argv[2];
 var numberOfRecords = Number(process.argv[3]);
-var uniqueTrainCounter =[];
-var finalTrain = [];
-var minutes = [];
-var finalMinutes = [];
-train = mongo.connect('mongodb://localhost:27017/trainSchedule', function(err, db) {
-	// console.log(db);
-	train = db.collection('trainData');
-	switch (caseTask) {
-		case '1' :
-		aggregateFunction(train,'distance', -1).toArray(function(error,docs) {
-			table.displayData(docs,'distance');
-		});
-		break;
 
-		case '2' :
-		aggregateFunction(train,'distance', 1).toArray(function(error,docs) {
-			table.displayData(docs, 'distance');
-		});
-		break;
-
-		case '3':
-			train.find().sort({counter : 1}).toArray(function(error,results) {
-				trainWithDuration = hourToMinute(results).sort(dynamicSort('operation'));;
-				for(i = 0; i < numberOfRecords; i += 1) {
-					finalTrain[i] = trainWithDuration[i];
-				}
-				table.displayData(finalTrain, 'duration');
-			})
-		break;
-
-		case '4':
-		var k =0;
-			train.find().sort({counter : 1}).toArray(function(error,results) {
-				trainWithDuration = hourToMinute(results).sort(dynamicSort('operation'));;
-				for(i = trainWithDuration.length - 1; i >= trainWithDuration.length - numberOfRecords; i -= 1) {
-					finalTrain[k] = trainWithDuration[i];
-					k += 1;
-				}
-				table.displayData(finalTrain, 'duration');
-			})
-		break;
-
-		case '5':
-		aggregateFunction(train,'numberOfStation' ,-1).toArray(function(error,docs) {
-			table.displayData(docs, 'number of station');
-		});
-		break;
-
-		case '6':
-		aggregateFunction(train,'numberOfStation' ,1).limit(numberOfRecords).toArray(function(error,docs) {
-			table.displayData(docs, 'number of station');
-		});
-		break;
-
-		case '7':
-		aggregateFunction(train,'visitedStation',-1).toArray(function(error,docs) {
-			table.displayData(docs, 'visited station');
-		});
-		break;
-
-		case '8':
-		aggregateFunction(train,'visitedStation',1).toArray(function(error,docs) {
-			table.displayData(docs, 'visited station');
-		});
-		break;
-
-		default :
-		console.log('Find and print 10 longest routes in terms of distance.');
-		console.log('Find and print 10 shortest routes in terms of distance.');
-		console.log('Find and print 10 longest routes in terms of duration.');
-		console.log('Find and print 10 shortest routes in terms of duration.');
-		console.log('Find and print 10 longest routes in terms of number of stations.');
-		console.log('Find and print 10 shortest routes in terms of number of stations.');
-		console.log('Find and print 10 most visited stations.');
-		console.log('Find and print 10 least visited stations.');
-
-	}
-
-
- });
+// -----sorting--------
 var dynamicSort = function (property) {
-		var sortOrder = 1;
-		if(property[0] === '-') {
-				sortOrder = -1;
-				property = property.substr(1);
-		}
-		return function (a,b) {
-				var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-				return result * sortOrder;
-		}
+	var sortOrder = 1;
+	if(property[0] === '-') {
+			sortOrder = -1;
+			property = property.substr(1);
+	}
+	return function (a,b) {
+			var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+			return result * sortOrder;
+	}
 }
+
+// --------convert hour to minutes--------
+var uniqueTrainCounter =[];
 var hourToMinute = function (results) {
 	for(i = 0; i < results.length; i += 1 ) {
 		// console.log(result)
@@ -142,6 +67,8 @@ var hourToMinute = function (results) {
 		}
 		return uniqueTrainCounter;
 }
+
+// -----------method to group the data and sort it.-------------
 var aggregateFunction = function (train,groupBy,orderBy) {
 	var trainGroupBy;
 	if (groupBy === 'distance') {
@@ -202,3 +129,102 @@ var aggregateFunction = function (train,groupBy,orderBy) {
 	}
 	return trainGroupBy.sort({operation : orderBy}).limit(numberOfRecords);
 }
+
+// ---------------convert minute to hour----------------
+var minutetohour = function(finalTrain) {
+	for(i = 0 ; i < finalTrain.length ; i++) {
+		m = (finalTrain[i].operation) % 60;
+		h = (finalTrain[i].operation - m)/60;
+		finalTrain[i].operation = h.toString() + ':' + (m < 10 ? '0' : '') + m.toString();
+		// console.log(finalTrain[i]);
+	}
+	return finalTrain;
+}
+
+train = mongo.connect('mongodb://localhost:27017/trainSchedule', function(err, db) {
+	train = db.collection('trainData');
+
+	var finalTrain = [];
+	switch (caseTask) {
+		case '1' :
+		// Find and print 10 longest routes in terms of distance.
+		aggregateFunction(train,'distance', -1).toArray(function(error,docs) {
+			table.displayData(docs,'distance');
+		});
+		break;
+
+		case '2' :
+		// Find and print 10 shortest routes in terms of distance.
+		aggregateFunction(train,'distance', 1).toArray(function(error,docs) {
+			table.displayData(docs, 'distance');
+		});
+		break;
+
+		case '3':
+		// Find and print 10 longest routes in terms of duration.
+		var k =0;
+		train.find().sort({counter : 1}).toArray(function(error,results) {
+			trainWithDuration = hourToMinute(results).sort(dynamicSort('operation'));;
+			for(i = trainWithDuration.length - 1; i >= trainWithDuration.length - numberOfRecords; i -= 1) {
+				finalTrain[k] = trainWithDuration[i];
+				k += 1;
+			}
+			finalTrain = minutetohour(finalTrain);
+			table.displayData(finalTrain, 'duration');
+		})
+		break;
+
+		case '4':
+		// Find and print 10 shortest routes in terms of duration.
+
+		train.find().sort({counter : 1}).toArray(function(error,results) {
+			trainWithDuration = hourToMinute(results).sort(dynamicSort('operation'));
+			for(i = 0; i < numberOfRecords; i += 1) {
+				finalTrain[i] = trainWithDuration[i];
+			}
+			finalTrain = minutetohour(finalTrain);
+			table.displayData(finalTrain, 'duration');
+		})
+		break;
+
+		case '5':
+		// Find and print 10 longest routes in terms of number of stations.
+		aggregateFunction(train,'numberOfStation' ,-1).toArray(function(error,docs) {
+			table.displayData(docs, 'number of station');
+		});
+		break;
+
+		case '6':
+		// Find and print 10 shortest routes in terms of number of stations.
+		aggregateFunction(train,'numberOfStation' ,1).limit(numberOfRecords).toArray(function(error,docs) {
+			table.displayData(docs, 'number of station');
+		});
+		break;
+
+		case '7':
+		// Find and print 10 most visited stations.
+		aggregateFunction(train,'visitedStation',-1).toArray(function(error,docs) {
+			table.displayData(docs, 'visited station');
+		});
+		break;
+
+		case '8':
+		// Find and print 10 least visited stations.
+		aggregateFunction(train,'visitedStation',1).toArray(function(error,docs) {
+			table.displayData(docs, 'visited station');
+		});
+		break;
+
+		default :
+		console.log('Find and print 10 longest routes in terms of distance.');
+		console.log('Find and print 10 shortest routes in terms of distance.');
+		console.log('Find and print 10 longest routes in terms of duration.');
+		console.log('Find and print 10 shortest routes in terms of duration.');
+		console.log('Find and print 10 longest routes in terms of number of stations.');
+		console.log('Find and print 10 shortest routes in terms of number of stations.');
+		console.log('Find and print 10 most visited stations.');
+		console.log('Find and print 10 least visited stations.');
+
+	}
+
+ });
